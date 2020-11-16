f22c76b4835f463521ff244d844661edb1e56c58
hongshuochen <hongshuo@usc.edu>
2020-11-16 03:09:03 -0600

    update tests

--- tests/trojanmap_test.cc
+++ tests/trojanmap_test.cc
@@ -9,16 +9,28 @@ TEST(TrojanMapTest, Autocomplete) {
   TrojanMap m;
   m.CreateGraphFromCSVFile();
   auto names = m.Autocomplete("Ch");
-  std::vector<std::string> gt = {"ChickfilA", "Chipotle Mexican Grill"};
-  EXPECT_EQ(names, gt);
+  std::vector<std::string> gt1 = {"ChickfilA", "Chipotle Mexican Grill"};
+  EXPECT_EQ(names, gt1);
+  names = m.Autocomplete("ch");
+  std::vector<std::string> gt2 = {"ChickfilA", "Chipotle Mexican Grill"};
+  EXPECT_EQ(names, gt2);
+  names = m.Autocomplete("cH");
+  std::vector<std::string> gt3 = {"ChickfilA", "Chipotle Mexican Grill"};
+  EXPECT_EQ(names, gt3);
 }
 
 TEST(TrojanMapTest, FindPosition) {
   TrojanMap m;
   m.CreateGraphFromCSVFile();
   auto position = m.GetPosition("ChickfilA");
-  std::pair<double, double> gt(34.0167334, -118.2825307);
-  EXPECT_EQ(position, gt);
+  std::pair<double, double> gt1(34.0167334, -118.2825307);
+  EXPECT_EQ(position, gt1);
+  position = m.GetPosition("Ralphs");
+  std::pair<double, double> gt2(34.0317653, -118.2908339);
+  EXPECT_EQ(position, gt2);
+  position = m.GetPosition("Target");
+  std::pair<double, double> gt3(34.0257016, -118.2843512);
+  EXPECT_EQ(position, gt3);
 }
 
 TEST(TrojanMapTest, CalculateShortestPath) {
@@ -26,21 +38,67 @@ TEST(TrojanMapTest, CalculateShortestPath) {
   m.CreateGraphFromCSVFile();
   auto path = m.CalculateShortestPath("Ralphs", "ChickfilA");
   std::vector<std::string> gt{
-      "2578244375", "5559640911", "6787470571", "6808093910", "6808093913",
-      "6808093919", "6816831441", "6813405269", "6816193784", "6389467806",
-      "6816193783", "123178876",  "2613117895", "122719259",  "2613117861",
-      "6817230316", "3642819026", "6817230310", "7811699597", "5565967545",
-      "123318572",  "6813405206", "6813379482", "544672028",  "21306059",
-      "6813379476", "6818390140", "63068610",   "6818390143", "7434941012",
-      "4015423966", "5690152766", "6813379440", "6813379466", "21306060",
-      "6813379469", "6813379427", "123005255",  "6807200376", "6807200380",
-      "6813379451", "6813379463", "123327639",  "6813379460", "4141790922",
-      "4015423963", "1286136447", "1286136422", "4015423962", "6813379494",
-      "63068643",   "6813379496", "123241977",  "4399914059", "4015372478",
-      "1732243576", "6813379548", "4015372476", "4015372475", "1732243620",
-      "4015372469", "4015372463", "6819179749", "1732243544", "6813405275",
-      "348121996",  "348121864",  "6813405280", "1472141024", "4015372462",
-      "6813411586", "4015372458", "6813411588", "1837212101", "6820935911",
-      "4547476733"};
+      "2578244375", "5559640911", "6787470571", "6808093910", "6808093913", "6808093919", "6816831441",
+      "6813405269", "6816193784", "6389467806", "6816193783", "123178876", "2613117895", "122719259",
+      "2613117861", "6817230316", "3642819026", "6817230310", "7811699597", "5565967545", "123318572",
+      "6813405206", "6813379482", "544672028", "21306059", "6813379476", "6818390140", "63068610", 
+      "6818390143", "7434941012", "4015423966", "5690152766", "6813379440", "6813379466", "21306060",
+      "6813379469", "6813379427", "123005255", "6807200376", "6807200380", "6813379451", "6813379463",
+      "123327639", "6813379460", "4141790922", "4015423963", "1286136447", "1286136422", "4015423962",
+      "6813379494", "63068643", "6813379496", "123241977", "4015372479", "4015372477", "1732243576",
+      "6813379548", "4015372476", "4015372474", "4015372468", "4015372463", "6819179749", "1732243544",
+      "6813405275", "348121996", "348121864", "6813405280", "1472141024", "6813411590", "216155217", 
+      "6813411589", "1837212103", "1837212101", "6820935911", "4547476733"};
+  EXPECT_EQ(path, gt);
+  
+  path = m.CalculateShortestPath("ChickfilA", "Ralphs");
+  std::reverse(gt.begin(),gt.end());
   EXPECT_EQ(path, gt);
 }
+
+TEST(TrojanMapTest, CalculateShortestPath2) {
+  TrojanMap m;
+  m.CreateGraphFromCSVFile();
+  auto path = m.CalculateShortestPath("Target", "Popeyes Louisiana Kitchen");
+  std::vector<std::string> gt{
+      "5237417650", "6813379479", "5237381975", "4399698012", "4399698013", "4399698011", "4399698010", 
+      "123044712", "4399698009", "4399698008", "123005253", "6813379513", "6813379517", "6813379521", 
+      "123327627", "4399697999", "6813565290", "122719210", "6813379407", "2613117879", "6813379406", 
+      "6807905595", "6787803635", "2613117867", "4835551110", "6813565296", "122719205", "6813565294", "4835551232", 
+      "4835551104", "4012842272", "4835551103", "123178841", "6813565313", "122814435", "6813565311", "4835551228", 
+      "6813513565", "4835551090", "4835551081", "6813513564", "20400292", "5556117120", "5556117115", "4835551064", 
+      "4012842277", "6813565326", "123241961", "6813565322", "4835551070", "5695236164"};
+  EXPECT_EQ(path, gt);
+  
+  path = m.CalculateShortestPath("Popeyes Louisiana Kitchen", "Target");
+  std::reverse(gt.begin(),gt.end());
+  EXPECT_EQ(path, gt);
+}
+
+TEST(TrojanMapTest, TSP) {
+  TrojanMap m;
+  m.CreateGraphFromCSVFile();
+  std::vector<std::string> input{"1873056015", "6905329551", "213332060", "1931345270"};
+  auto result = m.TravellingTrojan(input);
+  std::vector<std::string> gt{"1873056015", "213332060", "1931345270", "6905329551", "1873056015"};
+  EXPECT_EQ(gt, result.second[result.second.size()-1]);
+}
+
+TEST(TrojanMapTest, TSP2) {
+  TrojanMap m;
+  m.CreateGraphFromCSVFile();
+  std::vector<std::string> input{"1862312636", "7424270441", "67666219", "4015405548", "4015203110", "6807439002"};
+  auto result = m.TravellingTrojan(input);
+  std::vector<std::string> gt{"1862312636", "4015405548", "4015203110", "6807439002", "7424270441", "67666219", "1862312636"};
+  EXPECT_EQ(gt, result.second[result.second.size()-1]);
+}
+
+TEST(TrojanMapTest, TSP3) {
+  TrojanMap m;
+  m.CreateGraphFromCSVFile();
+  std::vector<std::string> input{"123120189", "4011837229", "4011837224", "2514542032", "2514541020", "1931345270", "4015477529", "214470792", "63068532", "6807909279"};
+  auto result = m.TravellingTrojan(input);
+  std::vector<std::string> gt{"123120189", "4011837224", "4011837229", "1931345270", "2514542032", "2514541020", "6807909279", "63068532", "214470792", "4015477529", "123120189"};
+  EXPECT_EQ(gt, result.second[result.second.size()-1]);
+}
+
